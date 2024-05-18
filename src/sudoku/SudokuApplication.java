d1b4b7161cfa661d9672fbd62fe26d9d3e21d52a
PuttipongMax <maxnetoogo@gmail.com>
2024-05-18 23:10:03 +0700

    Application Container/Entry Point (SudokuApplication, Main)

---
+++ src/sudoku/SudokuApplication.java
@@ -0,0 +1,27 @@
+package sudoku;
+
+import java.io.IOException;
+
+import javafx.application.application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class SudokuApplication extends Application{
+ private IUserInterfaceContract.View uiImpl;
+ 
+ @Override
+ public void start(Stage primaryStage) throws Exception {
+  uiImpl = new UserInterfaceImpl(primaryStage);
+  try{
+   SudokuBuildLogic.build(uiImpl);
+  }
+  catch(IOException e){
+   e.printStackTrace();
+   throw e;
+  }
+ }
+
+ public static void main(String[] args){
+  launch(args);
+ }
+}
